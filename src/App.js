2c684c17c95c441fa9b800fd4cd3f1de5ad8486b
Emilie Leury <emilie.leury@gmail.com>
2022-02-09 16:40:20 +0100

    09022022 16:40 — MAJ

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
+// import of axios to do some API's request
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
+  /*-----------------------------------------------------------------------*/
+
+  //il faut importer de la data de l'API et la stocker:
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  /*-----------------------------------------------------------------------*/
+  useEffect(() => {
+    // fonction a effectuer à chaque arriver sur la page
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+  // on utilise un tab vide dans le useEffet pour effectuer cette action au composant une seule fois, à sa création
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
     <div className="App">
       {/* STRUTURE DU SITE 
-      Router is the navigation parent's
-    Routes contains the page's list
-    Route is the component that contains the page */}
+  Router is the navigation parent's
+Routes contains the page's list
+Route is the component that contains the page */}
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/offer/:id" element={<Offer />} />
+          <Route path="/" element={<Home data={data} />} />
+          <Route path="/offer/:id" element={<Offer data={data} />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Offer.js
+++ src/pages/Offer.js
@@ -1,10 +1,18 @@
 import { useParams } from "react-router-dom";
 // useful to use the params in the URL
 
-const Offer = () => {
+const Offer = ({ data }) => {
   const { id } = useParams();
-  console.log(id);
-  return <div>J'ai récupéré l'id : {id}</div>;
+  console.log("L'id du produit est :", id);
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        L'id de {data.offers.product_name} est : {id}
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
